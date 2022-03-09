fbd61fdbe43e78e4181c568f4d3fca44562eaedb
SangWoo Nam <nsw5296@naver.com>
2022-03-09 19:33:59 +0900

    [Add] nGrinder - refresh token login API performance test script

---
+++ performance/refresh-token-login.groovy
@@ -0,0 +1,64 @@
+import static net.grinder.script.Grinder.grinder
+import static org.junit.Assert.*
+import static org.hamcrest.Matchers.*
+import net.grinder.script.GTest
+import net.grinder.script.Grinder
+import net.grinder.scriptengine.groovy.junit.GrinderRunner
+import net.grinder.scriptengine.groovy.junit.annotation.BeforeProcess
+import net.grinder.scriptengine.groovy.junit.annotation.BeforeThread
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.ngrinder.http.HTTPRequest
+import org.ngrinder.http.HTTPRequestControl
+import org.ngrinder.http.HTTPResponse
+import org.ngrinder.http.cookie.Cookie
+import org.ngrinder.http.cookie.CookieManager
+
+import HTTPClient.NVPair
+
+@RunWith(GrinderRunner)
+class TestRunner {
+
+	public static GTest test
+	public static HTTPRequest request
+	public static NVPair[] headers = []
+	public static List<Cookie> cookies = []
+
+	@BeforeProcess
+	public static void beforeProcess() {
+		HTTPRequestControl.setConnectionTimeout(300000)
+		test = new GTest(1, "Refresh Token Login Performance Test")
+		request = new HTTPRequest()
+		grinder.logger.info("before process.")
+	}
+
+	@BeforeThread
+	public void beforeThread() {
+		test.record(this, "test")
+		grinder.statistics.delayReports = true
+		grinder.logger.info("before thread.")
+	}
+
+	@Before
+	public void before() {
+		headers = [ new NVPair("Content-type", "application/json;charset=UTF-8") ]
+		request.setHeaders(headers)
+		CookieManager.addCookies(cookies)
+		grinder.logger.info("before. init headers and cookies")
+	}
+
+	@Test
+	public void test() {
+		def map = [refreshToken : "리프레쉬 토큰", deviceCode : "deviceCode"]
+		HTTPResponse response = request.POST("http://gunimon.iptime.org:8081/api/oauth/login/refresh", map)
+
+		if (response.statusCode == 301 || response.statusCode == 302) {
+			grinder.logger.warn("Warning. The response may not be correct. The response code was {}.", response.statusCode)
+		} else {
+			assertThat(response.statusCode, is(200))
+		}
+	}
+}
